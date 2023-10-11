0a2fc05eb29c62e825c0bb629e5dd840a292adf3
Adam <81664765+wacsvn@users.noreply.github.com>
2023-10-10 18:17:26 -0700

    added QueryBuilder method
    adds query-building functionality by passing in the query type and target and automating the query execution process

--- databases.py
+++ databases.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 # Read in data files
 with open('Co2.html') as f:
     co2_data = f.read()
+
 with open('SeaLevel.csv') as f:
     sealevel_data = f.read()
 
@@ -15,6 +16,7 @@ co2_dict = {}
 
 for row in co2_table.find_all('tr')[3:]:
     columns = row.find_all('td')
+
     if len(columns) >= 7:
         year = int(columns[0].text)
         month = int(columns[1].text)
@@ -24,7 +26,6 @@ for row in co2_table.find_all('tr')[3:]:
         trend = float(columns[5].text)
         days = int(columns[6].text)
         co2_dict[(year, month)] = (decimal, avg, inter, trend, days)
-
 
 # Print CO2 data for debugging
 print("CO2 Data:")
@@ -36,24 +37,27 @@ else:
 
 # Parse sea level data
 sealevel_dict = {}
+
 for line in sealevel_data.splitlines()[4:]:
     parts = line.split(',')
+
     if len(parts) >= 2:
         date = parts[0]
         sea_level = float(parts[1])
-
         j1 = None
+
         if len(parts) >= 3:
             try:
                 j1 = float(parts[2])
             except ValueError:
                 j1 = None
+
         j2 = None
         j3 = None
         sealevel_dict[date] = (sea_level, j1, j2, j3)
 
-class Database:
 
+class Database:
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
         self.cur = self.conn.cursor()
@@ -61,7 +65,7 @@ class Database:
     def create_tables(self):
         self.cur.execute('''
             CREATE TABLE IF NOT EXISTS co2 (
-                year INTEGER, 
+                year INTEGER,
                 month INTEGER,
                 decimal REAL,
                 average REAL,
@@ -74,7 +78,7 @@ class Database:
 
         self.cur.execute('''
             CREATE TABLE IF NOT EXISTS sealevel (
-                date TEXT, 
+                date TEXT,
                 sea_level REAL,
                 j1 REAL,
                 j2 REAL,
@@ -85,16 +89,20 @@ class Database:
 
     def insert_co2(self, data):
         sql = 'INSERT INTO co2 (year, month, decimal, average, inter, trend, days) VALUES (?, ?, ?, ?, ?, ?, ?)'
+
         for key, val in data.items():
             values = (key[0], key[1], *val)
             self.cur.execute(sql, values)
+
         print("CO2 Data inserted")  # Debug print
 
     def insert_sealevel(self, data):
         sql = 'INSERT INTO sealevel (date, sea_level, j1, j2, j3) VALUES (?, ?, ?, ?, ?)'
+
         for key, val in data.items():
             values = (key, *val)
             self.cur.execute(sql, values)
+
         print("Sea Level Data inserted")  # Debug print
 
     def search_co2(self, year, month):
@@ -125,8 +133,48 @@ class Database:
     def commit(self):
         self.conn.commit()
 
-# Create database and tables
-db = Database('climate_data4.db')
+    def QueryBuilder(self, Data_Base, Query_Type, Query_Tuple):
+        '''
+        Build a query string based on the given parameters and execute it in the database.
+
+        Args:
+            Data_Base (str): The name of the database table. (really this should've been called table)
+            Query_Type (str): The type of query (e.g., "INSERT", "DELETE", "SELECT", "TABLE", "CREATE").
+            Query_Tuple (tuple): A tuple containing the data for the query.
+
+        Returns:
+            str: The constructed query string.
+        '''
+        if Query_Type == "INSERT":
+            query_string = f"INSERT INTO {Data_Base} VALUES {Query_Tuple}"
+        elif Query_Type == "DELETE":
+            query_string = f"DELETE FROM {Data_Base} WHERE {Query_Tuple[0]} = {Query_Tuple[1]}"
+        elif Query_Type == "SELECT":
+            query_string = f"SELECT * FROM {Data_Base} WHERE {Query_Tuple[0]} = {Query_Tuple[1]}"
+        elif Query_Type == "SEARCH":
+            query_string = f"SELECT * FROM {Data_Base} WHERE {Query_Tuple[0]} = {Query_Tuple[1]}'"
+        elif Query_Type == "TABLE":
+            # Assume Query_Tuple is a tuple of attribute names and data types
+            attributes = ", ".join(f"{attr} {data_type}" for attr, data_type in Query_Tuple)
+            query_string = f"CREATE TABLE IF NOT EXISTS {Data_Base} ({attributes})"
+        elif Query_Type == "CREATE":
+            # Assume Query_Tuple is a tuple of attribute names and data types
+            attributes = ", ".join(f"{attr} {data_type}" for attr, data_type in Query_Tuple)
+            query_string = f"CREATE TABLE IF NOT EXISTS {Data_Base} ({attributes})"
+        else:
+            # Invalid query type, raise an exception
+            raise ValueError(f"Unsupported query type: {Query_Type}")
+
+        # Execute the query
+        self.cur.execute(query_string)
+
+        # Return the constructed query string
+        return query_string
+
+
+# TESTING
+# Create database and tables for testing
+db = Database('climate_data.db')
 db.create_tables()
 
 # Insert data
@@ -135,29 +183,37 @@ db.insert_sealevel(sealevel_dict)
 
 # Commit changes
 db.commit()
-
 print('\nData imported to database!')
 
-# Search Testing
-# For co2
-result = db.search_co2(1959, 1)
-if result:
-    print("CO2 data found:")
-    for row in result:
-        print(row)
-else:
-    print("No matching CO2 data found.")
+# Test cases for the updated QueryBuilder method within the Database class
+def test_query_builder():
+    # Test INSERT query
+    insert_query = db.QueryBuilder("co2", "INSERT", (2023, 9, 408.99, 409.21, 408.65, 408.82, 30))
+    print("INSERT Query:", insert_query)
 
-# For sealevel
-result = db.search_sealevel('1992.9614')
-if result:
-    print("Sea level data found:")
-    print(result)
-else:
-    print("No matching sea level data found.")
+    # Test DELETE query
+    delete_query = db.QueryBuilder("co2", "DELETE", ("year", 2000))
+    print("DELETE Query:", delete_query)
 
-# Delete Testing
-db.delete_co2('1959-01')
-db.delete_sealevel('1992.9614')
+    # Test SELECT query
+    select_query = db.QueryBuilder("co2", "SELECT", ("year", 2003))
+    print("SELECT Query:", select_query)
 
-# ^ Deletes first rows of each table
+    # Test CREATE query
+    table_attributes = (
+        ("id", "INTEGER PRIMARY KEY"),
+        ("name", "TEXT NOT NULL"),
+        ("age", "INTEGER")
+    )
+    create_query = db.QueryBuilder("my_table", "CREATE", table_attributes)
+    print("CREATE Query:", create_query)
+
+    # To ensure that this code works, this CREATE query should have resulted in the climate_data.db file containing a third table alongside co2 and sealevel, called my_table.
+
+    db.commit()
+
+# Run the test cases
+test_query_builder()
+
+
+
